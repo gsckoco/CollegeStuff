99b9de9db1db86f0146b5682c8c90245e812165b
Ben Johnston <gsckmp4@outlook.com>
2021-11-16 14:24:21 +0000

    Add files via upload

---
+++ BookTaxi/BookTaxi/frmMainMenu.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace BookTaxi
+{
+    partial class frmMainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBookingNumber = new System.Windows.Forms.Label();
+            this.txtBookingNumber = new System.Windows.Forms.TextBox();
+            this.lblCustomerName = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.txtStartLocation = new System.Windows.Forms.TextBox();
+            this.lblStartLocation = new System.Windows.Forms.Label();
+            this.txtEndLocation = new System.Windows.Forms.TextBox();
+            this.lblEndLocation = new System.Windows.Forms.Label();
+            this.lstAllBookings = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblViewAll = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblBookingNumber
+            // 
+            this.lblBookingNumber.AutoSize = true;
+            this.lblBookingNumber.Location = new System.Drawing.Point(7, 24);
+            this.lblBookingNumber.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblBookingNumber.Name = "lblBookingNumber";
+            this.lblBookingNumber.Size = new System.Drawing.Size(118, 20);
+            this.lblBookingNumber.TabIndex = 0;
+            this.lblBookingNumber.Text = "Booking number";
+            // 
+            // txtBookingNumber
+            // 
+            this.txtBookingNumber.Location = new System.Drawing.Point(11, 48);
+            this.txtBookingNumber.Name = "txtBookingNumber";
+            this.txtBookingNumber.Size = new System.Drawing.Size(162, 28);
+            this.txtBookingNumber.TabIndex = 1;
+            // 
+            // lblCustomerName
+            // 
+            this.lblCustomerName.AutoSize = true;
+            this.lblCustomerName.Location = new System.Drawing.Point(7, 79);
+            this.lblCustomerName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCustomerName.Name = "lblCustomerName";
+            this.lblCustomerName.Size = new System.Drawing.Size(117, 20);
+            this.lblCustomerName.TabIndex = 2;
+            this.lblCustomerName.Text = "Customer Name";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(11, 102);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(162, 28);
+            this.txtCustomerName.TabIndex = 3;
+            // 
+            // txtStartLocation
+            // 
+            this.txtStartLocation.Location = new System.Drawing.Point(12, 156);
+            this.txtStartLocation.Name = "txtStartLocation";
+            this.txtStartLocation.Size = new System.Drawing.Size(162, 28);
+            this.txtStartLocation.TabIndex = 5;
+            // 
+            // lblStartLocation
+            // 
+            this.lblStartLocation.AutoSize = true;
+            this.lblStartLocation.Location = new System.Drawing.Point(8, 133);
+            this.lblStartLocation.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStartLocation.Name = "lblStartLocation";
+            this.lblStartLocation.Size = new System.Drawing.Size(111, 20);
+            this.lblStartLocation.TabIndex = 4;
+            this.lblStartLocation.Text = "Start Location";
+            // 
+            // txtEndLocation
+            // 
+            this.txtEndLocation.Location = new System.Drawing.Point(11, 212);
+            this.txtEndLocation.Name = "txtEndLocation";
+            this.txtEndLocation.Size = new System.Drawing.Size(162, 28);
+            this.txtEndLocation.TabIndex = 7;
+            // 
+            // lblEndLocation
+            // 
+            this.lblEndLocation.AutoSize = true;
+            this.lblEndLocation.Location = new System.Drawing.Point(7, 189);
+            this.lblEndLocation.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEndLocation.Name = "lblEndLocation";
+            this.lblEndLocation.Size = new System.Drawing.Size(98, 20);
+            this.lblEndLocation.TabIndex = 6;
+            this.lblEndLocation.Text = "End Location";
+            // 
+            // lstAllBookings
+            // 
+            this.lstAllBookings.FormattingEnabled = true;
+            this.lstAllBookings.ItemHeight = 20;
+            this.lstAllBookings.Location = new System.Drawing.Point(6, 27);
+            this.lstAllBookings.Name = "lstAllBookings";
+            this.lstAllBookings.Size = new System.Drawing.Size(287, 244);
+            this.lstAllBookings.TabIndex = 9;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(11, 246);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 29);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(99, 246);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 29);
+            this.btnSave.TabIndex = 11;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblViewAll
+            // 
+            this.lblViewAll.Location = new System.Drawing.Point(6, 284);
+            this.lblViewAll.Name = "lblViewAll";
+            this.lblViewAll.Size = new System.Drawing.Size(75, 29);
+            this.lblViewAll.TabIndex = 12;
+            this.lblViewAll.Text = "View All";
+            this.lblViewAll.UseVisualStyleBackColor = true;
+            this.lblViewAll.Click += new System.EventHandler(this.lblViewAll_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(11, 284);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 29);
+            this.btnExit.TabIndex = 13;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblBookingNumber);
+            this.groupBox1.Controls.Add(this.btnExit);
+            this.groupBox1.Controls.Add(this.txtBookingNumber);
+            this.groupBox1.Controls.Add(this.lblCustomerName);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.txtCustomerName);
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.lblStartLocation);
+            this.groupBox1.Controls.Add(this.txtStartLocation);
+            this.groupBox1.Controls.Add(this.lblEndLocation);
+            this.groupBox1.Controls.Add(this.txtEndLocation);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 325);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "New Booking";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lstAllBookings);
+            this.groupBox2.Controls.Add(this.lblViewAll);
+            this.groupBox2.Location = new System.Drawing.Point(218, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(305, 325);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Bookings";
+            // 
+            // frmMainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 351);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 11F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmMainMenu";
+            this.Text = "Taxi Booking";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBookingNumber;
+        private System.Windows.Forms.TextBox txtBookingNumber;
+        private System.Windows.Forms.Label lblCustomerName;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.TextBox txtStartLocation;
+        private System.Windows.Forms.Label lblStartLocation;
+        private System.Windows.Forms.TextBox txtEndLocation;
+        private System.Windows.Forms.Label lblEndLocation;
+        private System.Windows.Forms.ListBox lstAllBookings;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button lblViewAll;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ BookTaxi/BookTaxi/frmMainMenu.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookTaxi
+{
+    public partial class frmMainMenu : Form
+    {
+        List<string[]> listAllBookings = new List<string[]>();
+        public frmMainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtBookingNumber.Text = null;
+            txtCustomerName.Text = null;
+            txtStartLocation.Text = null;
+            txtEndLocation.Text = null;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string strBookingNumber = txtBookingNumber.Text;
+            string strCustomerName = txtCustomerName.Text;
+            string strStartLocation = txtStartLocation.Text;
+            string strEndLocation = txtEndLocation.Text;
+            btnClear_Click(sender, e);
+            string[] row = { strBookingNumber, strCustomerName, strStartLocation, strEndLocation };
+            listAllBookings.Add(row);
+            MessageBox.Show("Booking has been saved", "Saved");
+
+            //btnSave.Hide();
+        }
+
+        private void lblViewAll_Click(object sender, EventArgs e)
+        {
+            lstAllBookings.Items.Clear();
+            for (int i = 0; i < listAllBookings.Count; i++)
+            {
+                string row = "#" + listAllBookings[i][0] + " " + listAllBookings[i][1] + " " + listAllBookings[i][2] + " -> " + listAllBookings[i][3];
+                lstAllBookings.Items.Add(row);
+            }
+        }
+    }
+}
